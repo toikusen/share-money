b14476d3697407d4fd70098a22a592e384e77ea8
sei <sei.tu@neutec.com.tw>
2026-08-31 12:20:03 +0800

    🐛 fix(deploy): remove looping HTTPS redirect

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
-        destination: 'https://sharemoney.cc/:path*',
-        permanent: true,
-      },
-    ]
-  },
+  /* config options here */
 };
 
 export default nextConfig;
